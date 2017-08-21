46de5aaf191c294772cf89f59d66d6f54ba6168b
freefly0706 <1741093834@qq.com>
2017-08-21 15:48:19 +0800

    Modify Reverse Integer algorithm

--- algorithms/ReverseInteger.py
+++ algorithms/ReverseInteger.py
@@ -12,13 +12,11 @@ class Solution(object):
         if y < 0:
             y = -y
         res = 0
-        while y // 10 != 0:
+        while y != 0:
             res = res * 10 + y % 10
             y //= 10
-        if y != 0:
-            res = res * 10 + y
-            if res > 2 ** 31 - 1:
-                return 0
+        if res > 2 ** 31 - 1:
+            return 0
         if x < 0:
             return -res
         return res
