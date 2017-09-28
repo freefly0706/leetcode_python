96065b835679aad19a5458ad6448669d2701cb2b
freefly0706 <1741093834@qq.com>
2017-09-28 16:59:53 +0800

    Modify Longest Common Prefix algorithm

--- algorithms/oneTofifty/LongestCommonPrefix.py
+++ algorithms/oneTofifty/LongestCommonPrefix.py
@@ -14,11 +14,10 @@ class Solution(object):
                 shortest_len = len(strs[i])
                 shortest_str_index = i
         res = strs[shortest_str_index]
-        while True:
-            i = 0
-            while i < len(strs):
-                if strs[i][:shortest_len] != res[:shortest_len]:
-                    shortest_len -= 1
-                    continue
-                i += 1
-            return res[:shortest_len]
+        i = 0
+        while i < len(strs):
+            if strs[i][:shortest_len] != res[:shortest_len]:
+                shortest_len -= 1
+                continue
+            i += 1
+        return res[:shortest_len]
