685d2a5c3fb20e505b926bf425c7dcfee82da87a
freefly0706 <1741093834@qq.com>
2017-10-20 18:41:54 +0800

    Add Remove Nth Node From End of List algorithm

---
+++ algorithms/one2fifty/RemoveNthNodeFromEndOfList.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        p = q = head
+        for i in range(n):
+            p = p.next
+        if not p:
+            return head.next
+        while p.next:
+            p = p.next
+            q = q.next
+        q.next = q.next.next
+        return head
