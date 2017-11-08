3e46f71c38f415de1fd0737dc377d6b737d01d1d
freefly0706 <1741093834@qq.com>
2017-11-08 16:16:51 +0800

    Modify Letter Combinations of a Phone Number algorithm

--- algorithms/one2fifty/LetterCombinationsOfAPhoneNumber.py
+++ algorithms/one2fifty/LetterCombinationsOfAPhoneNumber.py
@@ -9,8 +9,8 @@ class Solution(object):
         res = []
         if digitsLength == 0:
             return res
-        digitCorString = {'2': "abc", '3': "def", '4': "ghi", '5': "jkl", '6': "mno", '7': "pqrs", '8': "tuv",
-                          '9': "wxyz"}
+        digitCorString = {'2': "abc", '3': "def", '4': "ghi", '5': "jkl",
+                          '6': "mno", '7': "pqrs", '8': "tuv", '9': "wxyz"}
 
         def backTracking(combination, index):
             if index == digitsLength:
