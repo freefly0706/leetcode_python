c42449995b80e4f4ec7a4081955ef0f8bba248f2
freefly0706 <1741093834@qq.com>
2017-11-24 23:01:45 +0800

    Add Search in Rotated Sorted Array algorithm

--- algorithms/one2fifty/SearchinRotatedSortedArray.py
+++ algorithms/one2fifty/SearchinRotatedSortedArray.py
@@ -1,1 +1,25 @@
 # -*- coding:utf-8 -*-
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                if nums[low] > nums[mid] and nums[high] < target:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[high] < nums[mid] and nums[low] > target:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return -1
