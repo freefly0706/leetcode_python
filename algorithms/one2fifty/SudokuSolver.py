877a2661f1a3344e1aa812eefc4c0ff81769dab5
freefly0706 <1741093834@qq.com>
2017-12-05 20:17:31 +0800

    Add Sudoku Solver algorithm

---
+++ algorithms/one2fifty/SudokuSolver.py
@@ -0,0 +1,36 @@
+# -*- coding:utf-8 -*-
+class Solution(object):
+    def solveSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: void Do not return anything, modify board in-place instead.
+        """
+        Solution().backTracking(board, 0, 0, len(board))
+
+    def backTracking(self, board, row, column, n):
+        if column == n:
+            return Solution().backTracking(board, row + 1, 0, n)
+        if row == n:
+            return True
+        if board[row][column] == '.':
+            for num in range(1, 10):
+                if Solution().isValid(board, row, column, str(num), n):
+                    board[row][column] = str(num)
+                    if Solution().backTracking(board, row, column + 1, n):
+                        return True
+                    board[row][column] = '.'
+            return False
+        return Solution().backTracking(board, row, column + 1, n)
+
+    def isValid(self, board, row, column, num, n):
+        for i in range(n):
+            if board[i][column] == num:
+                return False
+            if board[row][i] == num:
+                return False
+        startRow, startColumn = 3 * (row // 3), 3 * (column // 3)
+        for i in range(1, 3):
+            for j in range(1, 3):
+                if board[startRow + (row + i) % 3][startColumn + (column + j) % 3] == num:
+                    return False
+        return True
