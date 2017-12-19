c933df40ba3ddb6260c5c8b79cfc2ad88b76ee39
freefly0706 <1741093834@qq.com>
2017-12-19 22:56:37 +0800

    Add Trapping Rain Water algorithm

--- algorithms/one2fifty/TrappingRainWater.py
+++ algorithms/one2fifty/TrappingRainWater.py
@@ -1,1 +1,20 @@
 # -*- coding:utf-8 -*-
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        if len(height) < 3:
+            return 0
+        contain = 0
+        left, right = 0, len(height) - 1
+        while left < right:
+            min_bound = min(height[left], height[right])
+            while left < right and height[left] <= min_bound:
+                contain += min_bound - height[left]
+                left += 1
+            while right > left and height[right] <= min_bound:
+                contain += min_bound - height[right]
+                right -= 1
+        return contain
