3d5be5f2d0a4b8b002d59303780f9325a9a18196
freefly0706 <1741093834@qq.com>
2017-12-19 22:56:17 +0800

    Modify Container With Most Water algorithm

--- algorithms/one2fifty/ContainerWithMostWater.py
+++ algorithms/one2fifty/ContainerWithMostWater.py
@@ -11,8 +11,8 @@ class Solution(object):
         while left < right:
             min_bound = min(height[left], height[right])
             max_value = max(max_value, min_bound * (right - left))
-            while left < len(height) and height[left] <= min_bound:
+            while left < right and height[left] <= min_bound:
                 left += 1
-            while right > 0 and height[right] <= min_bound:
+            while right > left and height[right] <= min_bound:
                 right -= 1
         return max_value
