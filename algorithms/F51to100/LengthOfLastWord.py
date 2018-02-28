80214ba97058f60021147bfdf2d223d737163a37
e <1741093834@qq.com>
2018-02-28 12:47:35 +0800

    Add Length of Last Word algorithm

---
+++ algorithms/F51to100/LengthOfLastWord.py
@@ -0,0 +1,8 @@
+# -*-coding:utf-8 -*-
+class Solution:
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return len(s.strip().split(' ')[-1])
